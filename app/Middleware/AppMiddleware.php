8bb5b966ac2ecf6bc107a01dc530dfcc0eb7944c
helinhan <463929412@qq.com>
2020-01-12 23:45:18 +0800

    重写404和405错误

--- app/Middleware/AppMiddleware.php
+++ app/Middleware/AppMiddleware.php
@@ -7,6 +7,7 @@ namespace App\Middleware;
 use App\Helper\CommonConstHelper;
 use App\Helper\Helper;
 use App\Helper\ResponseHelper;
+use Hyperf\HttpMessage\Stream\SwooleStream;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -48,12 +49,37 @@ class AppMiddleware implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        //请求控制器前
+        /**请求之前**/
         $response = $handler->handle($request);
-        //请求之后-设置全局统一的返回格式
-        $contents = $response->getBody()->getContents();
+
+        /**请求之后**/
+        //响应对象
         $body = new ResponseHelper(CommonConstHelper::CODE_STATUS_SUCCESS);
-        $body->setData(Helper::jsonDecode($contents));
+
+        //重写404错误
+        $statusCode = $response->getStatusCode();
+        if ($statusCode == 404){
+            $body->setCode(404);
+            $body->setMsg(CommonConstHelper::HTTP_STATUS_PAGE_NOT_FOUND_MSG);
+            return $response->withStatus(404)->withBody(new SwooleStream(Helper::jsonEncode($body->getResponse())));
+        }
+
+        //重写405错误
+        $statusCode = $response->getStatusCode();
+        if ($statusCode == 405){
+            $body->setCode(405);
+            $body->setMsg(CommonConstHelper::HTTP_STATUS_METHOD_NOT_ALLOWED_MSG);
+            return $response->withStatus(405)->withBody(new SwooleStream(Helper::jsonEncode($body->getResponse())));
+        }
+
+        //设置全局统一的返回格式
+        $contents = $response->getBody()->getContents();
+
+        //没有返回的数据默认返回空对象
+        if ($contents){
+            $body->setData(Helper::jsonDecode($contents));
+        }
+
         return $this->response->json($body->getResponse());
     }
 }
